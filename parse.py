68e5b7fb77c2ddec94a2c4f2e77ba5cac9a74df5
Fache Aurélien <aurelien.fache@gmail.com>
2017-06-26 13:14:36 +0200

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+import json
+
+json_file='labs.json'
+
+json_data=open(json_file)
+data = json.load(json_data)
+json_data.close()
+
+total = len(data['features'])
+
+for i in range(1,total):
+	print data['features'][i]['properties']['website']
